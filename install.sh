a1861033663295967e83069eb12d801960396b42
1emvr <1emvr@protonmail.com>
2023-07-31 01:33:42 -0400

    ??

--- install.sh
+++ install.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
+cp configs/.xinitrc ~
+cp configs/.zshrc ~
+
+cp -r kitty ~/.configs/
+cp -r nvim ~/.configs/
+
 cp config.def.h config.h && sudo make clean install && sudo pkill -u $USER
