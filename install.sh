d7080e951dddf73508cc12b03033585fd3339947
1emvr <1emvr@protonmail.com>
2023-07-31 01:34:40 -0400

    ??

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 cp configs/.xinitrc ~
 cp configs/.zshrc ~
 
-cp -r kitty ~/.configs/
-cp -r nvim ~/.configs/
+cp -r configs/kitty ~/.configs/
+cp -r configs/nvim ~/.configs/
 
 cp config.def.h config.h && sudo make clean install && sudo pkill -u $USER
